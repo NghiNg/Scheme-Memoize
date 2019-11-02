73291d5665f37db75825646e9b1dac639d84e94f
unknown <Nghi_96@hotmail.com>
2019-11-02 13:37:01 +0100

    AAhhh

--- oblig3a.scm
+++ oblig3a.scm
@@ -2,7 +2,7 @@
 
 
 ;============= OPPGAVE 1A ================
-
+#|
 (define (mem message proc)
   (let ((table (make-table)))
     (lambda args
@@ -11,21 +11,9 @@
           (let ((result (apply proc args)))            
             (insert! args result table)
             result)))))
+|#
 
 
-;Test:
-;(set! fib (mem 'memoize fib))
-;(fib 3)
-;(fib 3)
-;(fib 2)
-;(fib 4)
-
-;(set! test-proc (mem 'memoize test-proc))
-;(test-proc)
-;(test-proc)
-;(test-proc 40 41 42 43 44)
-;(test-proc 40 41 42 43 44)
-;(test-proc 42 43 44)
 
 
 
@@ -35,15 +23,12 @@
 ;(define (orig-proc proc)
  ; (set! orig-proc proc))
 
-(define (mem2 message proc)
+(define (mem message proc)
     
     (let ((orig-proc proc))
              (set! orig-proc proc)
 
     (cond ((eq? message 'memoize)
-
-           
-
            (let ((table (make-table)))       
              (lambda args
                (if (not (eq? #f (lookup args table)))
@@ -55,24 +40,51 @@
           ((eq? message 'unmemoize) orig-proc)
           
           (else 'Error))))
+#|
+(define (mem message proc)
+    
+    (let ((orig-proc proc))
+             (set! orig-proc proc)
 
+    (cond ((eq? message 'memoize)
+           (let ((table (make-table)))       
+             (lambda args
+               (if (not (eq? #f (lookup args table)))
+                   (lookup args table)           
+                   (insert! args (apply proc args) table))
+               (apply proc args))))
+
+          ((eq? message 'unmemoize) orig-proc)
+          
+          (else 'Error))))
+|#
 
 
 ;Test:
 "AAAAHHHHH"
-(set! fib (mem2 'memoize fib))
+#|
+(set! fib (mem 'memoize fib))
 (fib 3)
 (fib 3)
 (fib 2)
 (fib 4)
 
-(set! fib (mem2 'unmemoize fib))
+(set! fib (mem 'unmemoize fib))
 (fib 3)
 
 
-(set! test-proc (mem2 'memoize test-proc))
+(set! test-proc (mem 'memoize test-proc))
 (test-proc)
 (test-proc)
 (test-proc 40 41 42 43 44)
 (test-proc 40 41 42 43 44)
 (test-proc 42 43 44)
+(set! test-proc (mem 'unmemoize test-proc))
+|#
+#|
+"BAAAAAHHH"
+(define mem-fib (mem 'memoize fib))
+(mem-fib 3)
+(mem-fib 3)
+(mem-fib 2)
+|#
